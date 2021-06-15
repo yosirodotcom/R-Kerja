ae893ede502ceab4d56a69a47e0620a6675ba44c
yosirodotcom <yosironadi@gmail.com>
2021-06-15 07:54:41 +0700

    adding files

---
+++ UPT Bahasa/rumus join nim.R
@@ -0,0 +1,52 @@
+library(tidyverse)
+library(rio)
+library(RODBC)
+
+
+setwd("C:/Users/ASUS/Desktop")
+
+dta <- odbcDriverConnect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:/Cloud Data/OneDrive - Universitas Terbuka/UPT Bahasa/2 Yosi Folder/Database Projects/UPT BAHASA Database Master.accdb")
+
+tabel1 <- sqlFetch(dta, "mhs_data_T")
+
+tabel2 <- read.delim(file = "clipboard", header = T) %>% as_tibble()
+
+# Cek duplikat NIM
+tabel2$nim[duplicated(tabel2$nim)]
+
+
+tabel1$tanggal_lahir <- as.Date(tabel1$tanggal_lahir, tz = "Asia/Bangkok", format = "%d-%m-%y")
+tabel2$nim <- as.character(tabel2$nim)
+tabel1$nama <- toupper(tabel1$nama)
+tabel1$tempat_lahir <- toupper(tabel1$tempat_lahir)
+tabel2$nama <- toupper(tabel2$nama)
+tabel_join <- left_join(tabel2, tabel1, by = "nim")
+tabel_join1 <- tabel_join %>% dplyr::select(nama.x, nim, tempat_lahir, tanggal_lahir, sex) %>% arrange(nama.x)
+
+
+
+tabel_join2 <- tabel_join1 %>% group_by(nim) %>% slice(1) %>% arrange(nama.x)
+
+
+
+# Export 1 by 1
+export(tabel_join2, file = "dkb.xlsx")
+
+# Join all file
+
+name_file <- as.list(sprintf("%s.xlsx", seq(1:5)))
+
+import_file <- lapply(name_file, import)
+export(import_file, "combine.xlsx")
+
+# book <- loadWorkbook("tabel_join.xlsx")
+# createSheet(book, kelas)
+# writeWorksheet(book, tabel_join2, sheet = kelas)
+# saveWorkbook(book, file = "tabel_join.xlsx")
+
+
+
+
+
+
+
---
+++ UPT Bahasa/rumus result ect.R
@@ -0,0 +1,103 @@
+
+# Load Packages
+library(tidyverse)
+library(rio)
+library(RODBC)
+
+# Set Working Directory
+setwd("C:/Users/ASUS/Desktop")
+
+
+# Connect to Database
+dta <- odbcDriverConnect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:/Cloud Data/OneDrive - Universitas Terbuka/UPT Bahasa/2 Yosi Folder/Database Projects/UPT BAHASA Database Master.accdb")
+
+# Load Tables
+tbl_mhs_data <- sqlFetch(dta, "mhs_data_T") %>% as_tibble()
+tbl_mhs_kelas <- sqlFetch(dta, "mhs_kelas_T") %>% as_tibble()
+tbl_dosen <- sqlFetch(dta, "dosen") %>% as_tibble()
+tbl_jurusan <- sqlFetch(dta, "jurusan") %>% as_tibble()
+tbl_pengawas <- sqlFetch(dta, "pengawas") %>% as_tibble()
+tbl_prodi <- sqlFetch(dta, "prodi") %>% as_tibble()
+tbl_mhs_daftar_toefl <- sqlFetch(dta, "mhs_daftar_toefl_T") %>% as_tibble()
+tbl_mhs_score_toefl <- sqlFetch(dta, "mhs_score_toefl_T") %>% as_tibble()
+tbl_mhs_daftar_toeic <- sqlFetch(dta, "mhs_daftar_toeic_T") %>% as_tibble()
+tbl_mhs_score_toeic <- sqlFetch(dta, "mhs_score_toeic_T") %>% as_tibble()
+tbl_rumus_toefl_grammar <- sqlFetch(dta, "rumus_toefl_grammar") %>% as_tibble()
+tbl_rumus_toefl_listening <- sqlFetch(dta, "rumus_toefl_listening") %>% as_tibble()
+tbl_rumus_toefl_reading <- sqlFetch(dta, "rumus_toefl_reading") %>% as_tibble()
+tbl_rumus_toeic_grammar <- sqlFetch(dta, "rumus_toeic_reading") %>% as_tibble()
+tbl_rumus_toeic_listening <- sqlFetch(dta, "rumus_toeic_listening") %>% as_tibble()
+tbl_rumus_toeic_grammar$ID <- as.numeric(tbl_rumus_toeic_grammar$ID)
+tbl_rumus_toeic_listening$ID <- as.numeric(tbl_rumus_toeic_listening$ID)
+
+glimpse(tbl_mhs_score_toeic)
+
+# Load Table Result
+b3 <- import("B3.xlsx") %>% as_tibble()
+b4 <- import("B4.xlsx") %>% as_tibble()
+b5 <- import("B5.xlsx") %>% as_tibble()
+b6 <- import("B6.xlsx") %>% as_tibble()
+b7 <- import("B7.xlsx") %>% as_tibble()
+b8 <- import("B8.xlsx") %>% as_tibble()
+b9 <- import("B9.xlsx") %>% as_tibble()
+b10 <- import("B10.xlsx") %>% as_tibble()
+
+
+# Simplify Table Resutl
+b3 <- b3 %>% dplyr::select(id_peserta = `No. Peserta`, l1 = `Section 1`, r1 = `Section 2`) 
+b3$l1 <- as.numeric(b3$l1)
+b3$r1 <- as.numeric(b3$r1)
+b3$id_peserta <- as.numeric(b3$id_peserta)
+b4 <- b4 %>% dplyr::select(id_peserta = `No. Peserta`, l1 = `Section 1`, r1 = `Section 2`) 
+b4$l1 <- as.numeric(b4$l1)
+b4$r1 <- as.numeric(b4$r1)
+b4$id_peserta <- as.numeric(b4$id_peserta)
+b5 <- b5 %>% dplyr::select(id_peserta = `No. Peserta`, l1 = `Section 1`, r1 = `Section 2`) 
+b5$l1 <- as.numeric(b5$l1)
+b5$r1 <- as.numeric(b5$r1)
+b5$id_peserta <- as.numeric(b5$id_peserta)
+b6 <- b6 %>% dplyr::select(id_peserta = `No. Peserta`, l1 = `Section 1`, r1 = `Section 2`) 
+b6$l1 <- as.numeric(b6$l1)
+b6$r1 <- as.numeric(b6$r1)
+b6$id_peserta <- as.numeric(b6$id_peserta)
+b7 <- b7 %>% dplyr::select(id_peserta = `No. Peserta`, l1 = `Section 1`, r1 = `Section 2`) 
+b7$l1 <- as.numeric(b7$l1)
+b7$r1 <- as.numeric(b7$r1)
+b7$id_peserta <- as.numeric(b7$id_peserta)
+b8 <- b8 %>% dplyr::select(id_peserta = `No. Peserta`, l1 = `Section 1`, r1 = `Section 2`) 
+b8$l1 <- as.numeric(b8$l1)
+b8$r1 <- as.numeric(b8$r1)
+b8$id_peserta <- as.numeric(b8$id_peserta)
+b9 <- b9 %>% dplyr::select(id_peserta = `No. Peserta`, l1 = `Section 1`, r1 = `Section 2`) 
+b9$l1 <- as.numeric(b9$l1)
+b9$r1 <- as.numeric(b9$r1)
+b9$id_peserta <- as.numeric(b9$id_peserta)
+b10 <- b10 %>% dplyr::select(id_peserta = `No. Peserta`, l1 = `Section 1`, r1 = `Section 2`) 
+b10$l1 <- as.numeric(b10$l1)
+b10$r1 <- as.numeric(b10$r1)
+b10$id_peserta <- as.numeric(b10$id_peserta)
+
+# combine result table (a table)
+b <- bind_rows(b3, b4, b5, b6, b7, b8, b9, b10) %>% replace_na(list(l1 = 0, r1 = 0))
+min_b <- b %>% summarize(min(id_peserta)) %>% pull()
+
+# simplify score ect table (b table)
+a <- tbl_mhs_score_toeic %>% dplyr::filter(ID >= min_b) %>% dplyr::select(id_peserta = ID, ISO = mhs_daftar_toeic_ID, centang)
+
+# Join a and b table
+result <- left_join(a, b, by = "id_peserta", suffix = c(".a", ".b")) 
+result <- result %>% mutate(l1 = replace_na(l1, 0), r1 = replace_na(r1, 0))
+result <- result %>% mutate(centang = ifelse(l1 > 0, 1, 0)) 
+
+# Calculate score
+tbl_score <- merge(result, tbl_rumus_toeic_listening, by.x = "l1", by.y = "ID", all.x = T) 
+tbl_score <- merge(tbl_score, tbl_rumus_toeic_grammar, by.x = "r1", by.y = "ID", all.x = T) 
+tbl_score %<>% as_tibble() %>% arrange(id_peserta)
+tbl_score <- tbl_score %>% mutate(skor = listening+reading) 
+tbl_score$listening <- as.numeric(tbl_score$listening)
+tbl_score$listening <- as.numeric(tbl_score$reading)
+tbl_score$listening <- as.numeric(tbl_score$skor)
+tbl_score <- tbl_score %>% mutate(l1 = ifelse(l1 == 0, NA, l1), r1 = ifelse(r1 == 0, NA, r1))
+tbl_score <- tbl_score %>% dplyr::select(ID = id_peserta, l1, r1, l2 = listening, r2 = reading, skor, hadir = centang) 
+
+export(tbl_score, file = "tbl_score.xlsx")
